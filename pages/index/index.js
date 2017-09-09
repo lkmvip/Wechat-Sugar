22f8010cf9c99a618ed9af6f5754d0107920d022
jiyr <594402100@qq.com>
2017-09-09 16:56:24 +0800

    change

--- pages/index/index.js
+++ pages/index/index.js
@@ -45,6 +45,39 @@ Page({
         supplyList: [
             {id:0,url:'/image/gf1.png'},
             {id:1,url:'/image/gf1.png'},
+        ],
+        scrollList: [
+            {id:0,url:'/image/gf.png'},
+            {id:1,url:'/image/gf.png'},
+            {id:2,url:'/image/gf.png'},
+            {id:3,url:'/image/gf.png'},
+            {id:4,url:'/image/gf.png'},
+            {id:5,url:'/image/gf.png'},
+            {id:6,url:'/image/gf.png'},
+            {id:7,url:'/image/gf.png'}
+        ],
+        goodsList: [
+            {   id:0,
+                url:'/image/gf.png',
+                title:"白熊化妆品，美肤多效面霜50g",
+                newPrice:"998",
+                oldPrice:"1999",
+                makeMoney: "500",
+            },
+            {   id:1,
+                url:'/image/gf.png',
+                title:"白熊化妆品，美肤多效面霜50g",
+                newPrice:"998",
+                oldPrice:"1999",
+                makeMoney: "500",
+            },
+            {   id:2,
+                url:'/image/gf.png',
+                title:"白熊化妆品，美肤多效面霜50g",
+                newPrice:"998",
+                oldPrice:"1999",
+                makeMoney: "500",
+            }
         ]
     },
 
