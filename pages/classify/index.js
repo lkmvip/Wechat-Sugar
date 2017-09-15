01f6f3c375fabe6dd86a70d20a03538482241750
jiyr <594402100@qq.com>
2017-09-15 10:23:12 +0800

    change

--- pages/classify/index.js
+++ pages/classify/index.js
@@ -6,13 +6,26 @@ Page({
             {name:'品牌',id:0},
             {name:'分类',id:1}
         ],
-        tabIdx: 0
+        tabIdx: 0,
+        classifyFlag: true,
+        listTab: [
+            {name:'化妆品',id:0},
+            {name:'洁面',id:1},
+            {name:'防晒',id:2},
+            {name:'男士',id:3}
+        ],
+        leftTab: 0,
     },
 
     handleClickGoods: function(e) {
-        console.log(e)
         this.setData({
             tabIdx : e.target.dataset.index
+        })
+    },
+
+    handleClickTabs: function(e) {
+        this.setData({
+            leftTab : e.target.dataset.index
         })
     }
 });
